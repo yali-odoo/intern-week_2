38d93a97ec64d331f8a4864ce37b85d1e5e5f8ac
yali-odoo <yali@odoo.com>
2022-06-13 16:42:22 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+{
+    'name':'Odoo Academy',
+    
+    'summary':"""Academy app to manage Training""",
+    
+    'description':"""
+        Academy Module to manage Training:
+        -Courses
+        -Sessions
+        -Attendees
+    """,
+    
+    'author':'Odoo',
+    
+    'websie':'https//www.odoo.com',
+    
+    'category':'Training',
+    
+    'version':'0.1',
+    
+    'depends':['base'],
+    
+    'data':[
+        
+    ],
+    
+    'demo':[
+        
+        
+    ],
+   
+}
